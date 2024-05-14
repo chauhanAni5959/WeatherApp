bf55d4387d194a5ec8377bf2ee596e4f2ebe3693
Ankit <anisingh565072@gmail.com>
2024-05-14 09:41:26 +0530

    Add the functionality of splash screen

---
+++ app/src/main/java/com/production/weatherapp/splashscreen.kt
@@ -0,0 +1,31 @@
+package com.production.weatherapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        we create a handler because it will show the splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, MainActivity::class.java);
+            startActivity(intent)
+//            After running the handler it will automated to the main screen by
+            finish()
+
+        }, 3000)
+    }
+}
